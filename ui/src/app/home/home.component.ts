fcd5f766b96f39d5fbbf574d88792b96ea2fc740
Praveen Kumar <praveenknp@gmail.com>
2020-08-24 19:44:17 +0530

    Home page HTML

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faUserGraduate,faUser,faClock,faStar,faStarHalfAlt,faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  faUserGraduate = faUserGraduate;
+  faUser = faUser;
+  faClock = faClock;
+  faStar = faStar;
+  faStarHalfAlt = faStarHalfAlt;
+  faSearch = faSearch;
 
   constructor() { }
 
