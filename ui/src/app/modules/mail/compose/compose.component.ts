1e5592f1d41cde3bc6dde1008ec1ab297eb52637
mahesh K S <mahesh25242@gmail.com>
2020-11-16 13:46:14 +0530

    message in progress

--- ui/src/app/modules/mail/compose/compose.component.ts
+++ ui/src/app/modules/mail/compose/compose.component.ts
@@ -3,6 +3,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MailService } from '../services/mail.service';
 import Notiflix from "notiflix";
 import { Router, ActivatedRoute } from '@angular/router';
+import { User, UserWithPagination } from 'src/app/lib/interfaces';
+import { concat, Observable, of, Subject } from 'rxjs';
+import { catchError, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
+import { UserService } from 'src/app/lib/services';
 
 @Component({
   selector: 'app-compose',
@@ -12,10 +16,15 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ComposeComponent implements OnInit {
   @Input() reply:boolean;
   composeFrm:FormGroup;
+  users$: Observable<UserWithPagination>;
+  userInput$ = new Subject<string>();
+
+  peopleLoading = false;
   constructor(private formBuilder: FormBuilder,
     private mailService: MailService,
     private router: Router,
-    private route:ActivatedRoute) { }
+    private route:ActivatedRoute,
+    private userService : UserService) { }
 
   sentMessage(){
     Notiflix.Loading.Pulse(`Sending...`);
@@ -42,12 +51,36 @@ export class ComposeComponent implements OnInit {
 
   get f() { return this.composeFrm.controls; }
 
+  trackByFn(item: User) {
+      return item.id;
+  }
+
+
+  private loadUsers() {
+      this.users$ = concat(
+          of([]), // default items
+          this.userInput$.pipe(
+              distinctUntilChanged(),
+              tap(() => this.peopleLoading = true),
+              switchMap(term => this.userService.fetchAllStudent(1, `q=${term}&status=1`).pipe(
+                  map(res=>{
+                    return res.data;
+                  }),
+                  catchError(() => of(null)), // empty list on error
+                  tap(() => this.peopleLoading = false)
+              ))
+          )
+      );
+  }
+
   ngOnInit(): void {
     this.composeFrm = this.formBuilder.group({
       recipients: [null, [ Validators.required]],
       subject: [null, [ Validators.required]],
       message: [null, [ Validators.required]],
     });
+
+    this.loadUsers();
   }
 
 }
