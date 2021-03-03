d4894258ce9431bf5d0a1c8375e2a53738927117
mahesh K S <mahesh25242@gmail.com>
2021-03-03 07:34:02 +0530

    c

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   siteName: 'mayatutor',
   tinyMceApi: 'n6mryrdzt1pclqabsvpbvw6mtym3kj9gpwjguof89d2sv52q',
-  baseUrl: 'https://mayatutor/api/public',
+  baseUrl: 'https://fiya.in/mayatutor/api/public',
   recaptchaKey: '6Ld2McEZAAAAABDjGwyJ63pD3ETCQkWCiZqEH39C',
   lumenSecret: 'gGns8qiSpmPTBhfWJfZfle2pQqFJB439zbgrHdqw',
-  siteAddress: 'http://localhost/mayatutor'
+  siteAddress: 'https://fiya.in/mayatutor'
 };
