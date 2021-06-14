a11914b3ce1c8a74268a68ce2a36fce4fea66b00
Praveen Kumar <praveenknp@gmail.com>
2021-06-15 02:14:37 +0530

    Ui

--- ui/src/app/how-it-works/how-it-works.component.ts
+++ ui/src/app/how-it-works/how-it-works.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadCrumbsService } from '../shared-module/components/bread-crumbs/bread-crumbs.component';
+import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-how-it-works',
   templateUrl: './how-it-works.component.html',
   styleUrls: ['./how-it-works.component.scss']
 })
+
 export class HowItWorksComponent implements OnInit {
 
   constructor(private breadCrumbsService: BreadCrumbsService) { }
-
+  
   ngOnInit(): void {
     this.breadCrumbsService.bcs$.next([
       {
