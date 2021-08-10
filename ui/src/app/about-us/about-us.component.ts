6e9752388d624140c6e430e64a8798066f740890
mahesh K S <mahesh25242@gmail.com>
2021-08-10 16:35:48 +0530

    c

--- ui/src/app/about-us/about-us.component.ts
+++ ui/src/app/about-us/about-us.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+import { GoogleObj, GoogleTransulateService } from '../lib/services';
 import { BreadCrumbsService } from '../shared-module/components/bread-crumbs/bread-crumbs.component';
-
+import { data } from './data';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
-
-  constructor(private breadCrumbsService: BreadCrumbsService) { }
+  data$: Observable<any>;
+  constructor(private breadCrumbsService: BreadCrumbsService,
+    private googleTransulateService: GoogleTransulateService) { }
 
   ngOnInit(): void {
     this.breadCrumbsService.bcs$.next([
@@ -20,6 +24,43 @@ export class AboutUsComponent implements OnInit {
         name: 'About Us',
       }
     ]);
+
+    this.data$ = this.googleTransulateService.lang.pipe(mergeMap(res=>{
+      if(res != 'en'){
+        try{
+          let q: string[] = [];
+
+          const googleObj: GoogleObj = {
+            q: [data.first, data.second, data.abt_text, data.reg_office_addr],
+            target: res
+          };
+          return this.googleTransulateService.googleTransulate(googleObj).pipe(map((trans:any)=>{
+            let data = {
+              first: trans.data.translations[0].translatedText,
+              second: trans.data.translations[1].translatedText,
+              abt_text: trans.data.translations[2].translatedText,
+              reg_office_addr: trans.data.translations[3].translatedText,
+            };
+            return data
+          }));
+        }catch(e){
+          console.log(e)
+        }
+
+
+
+        // let q: string[] = [];
+        // this.data.map(res=>{
+        //   q = [...q, res.]
+        // })
+
+
+      }else{
+        return of(data)
+      }
+
+    }))
+
   }
 
 }
---
+++ ui/src/app/about-us/data.ts
@@ -0,0 +1,14 @@
+export const data = {
+  abt_text: `About mayatutors.com`,
+  reg_office_addr: `REGISTERED OFFICE ADDRESS`,
+  first: `<p>http//mayatutors.com is a web portal intended for educational purposes only.
+        The aim of this web portal is to provide a digital platform for teachers who wish to take tuition classes for different subjects online. Through this platform, teachers can conduct live classes as well as robotic classes which use recorded videos.
+
+      </p>
+      <p>Mayatutors.com accepts fees only to help teachers take tuition by displaying video, PDF, Excel, Word and live classes. Students can use this portal for free in order to find teachers, attend their classes after paying fees directly to the teachers.</p>
+      <p>As there are many teachers in this portal, students will have the opportunity to choose the best teacher. Teachers have the opportunity to display their videos, PDFs and conduct LIVE classes for a minimal fee. However, the service in mayatutors.com for students is free.</p>`,
+  second: `<p>Over the past nine years, (since 2013) Maya have been successful in making teacher's work easier and giving opportunities to students to study independently. When the whole world was suddenly struck by the covid pandemic, we managed to successfully run one year's full courses in "ROBOTIC" method without teachers. In fact it was a "face to face" virtual class. in which no matter how many students come to class at the same time, there will be a teacher in front of them on various subjects.</p>
+            <p>When this method of study is successful, we modified this website with the intention that ordinary tution teachers also should get this facility especially in this time of covid pandemic. In this age where education is falling into the hands of monopolies, talented teachers will have to build their own websites at huge cost to avoid losing their jobs. This web portal is for such teachers to use as  their own website.</p>
+            <p>So, whatever your knowledge, you can use this portal to impart that knowledge to all students. </p>
+            <p>Maya is legally and officially known as the MAYA TAX ACCOUNTING TRAINING CENTER.</p>`
+}
